1d26424bbee9b3d7eb2ce4b9550e093c25a4aa79
Pascal de Vink <pascal@pascaldevink.nl>
2022-05-15 14:59:34 +0200

    First version of publishing to Micro.blog
    Publishes to Micro.blog using the app token from settings, the name of
    the file as name of the entry, and the content without comments or tags.

--- src/microblog-client.ts
+++ src/microblog-client.ts
@@ -1,4 +1,4 @@
-import { App, MarkdownView, Notice } from "obsidian";
+import { App, MarkdownView, Notice, requestUrl, RequestUrlParam } from "obsidian";
 import MicroBlogPlugin from "./main";
 import { MicroBlogFrontMatter } from "./types";
 
@@ -15,26 +15,49 @@ export class MicroBlogClient {
 
         const { workspace } = this.app;
         const activeView = workspace.getActiveViewOfType(MarkdownView);
-        if (activeView) {
-            try {
-                const fileContent = await this.app.vault.cachedRead(activeView.file);
-                const frontMatter = this.app.metadataCache.getFileCache(activeView.file)?.frontmatter as MicroBlogFrontMatter;
+        if (!activeView) {
+            new Notice('There is no editor found. Nothing will be published.');
+            return;
+        }
 
-                if (!frontMatter) {
-                    new Notice('Please write frontmatter.');
-                    return;
-                }
+        try {
+            const name = activeView.file.basename
+            const fileContent = await this.app.vault.cachedRead(activeView.file);
+            const frontMatter = this.app.metadataCache.getFileCache(activeView.file)?.frontmatter as MicroBlogFrontMatter;
 
-                console.log(frontMatter?.status);
-                console.log(this.app.metadataCache.getFileCache(activeView.file)?.headings);
-                console.log(this.app.metadataCache.getFileCache(activeView.file)?.tags);
-                console.log(activeView.file.name);
-            } catch (ex: any) {
-                console.warn(ex);
-                new Notice(ex.toString());
+            if (!frontMatter) {
+                new Notice('Please write frontmatter.');
+                return;
             }
-        } else {
-          new Notice('There is no editor found. Nothing will be published.');
+
+            const status = frontMatter?.status
+            
+            const parsedContent = fileContent
+                .slice(frontMatter.position.end.offset)
+                .replace(/^<!--.*-->$/ms, '') // remove comments
+                .replace(/\#[\w/]+/, '') // remove tags
+                .trim();
+
+            const request: RequestUrlParam = {
+                url: 'https://micro.blog/micropub',
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + this.plugin.settings.appToken,
+                },
+            };
+            request.body = 'h=entry&post-status=' + status + '&name=' + name + '&content=' + parsedContent
+
+            console.log(request)
+
+            requestUrl(request).then(response => {
+                console.log(response)
+                console.log(response.json)
+                // @todo put publish url in frontmatter
+            })
+
+        } catch (ex: any) {
+            console.warn(ex);
+            new Notice(ex.toString());
         }
     }
 }
